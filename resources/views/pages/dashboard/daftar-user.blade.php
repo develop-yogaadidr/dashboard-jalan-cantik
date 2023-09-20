f270eec93bd90af27185e84158958e3fbb0f4a6a
Yoga Adi Dharma <yoga.dharma@formulatrix.com>
2023-09-20 08:53:22 +0700

    feat: pagination nav

--- resources/views/pages/dashboard/daftar-user.blade.php
+++ resources/views/pages/dashboard/daftar-user.blade.php
@@ -1,106 +1,99 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<x-card>
-    <div class="table-responsive">
-        <table id="table" class="display table" style="width:100%">
-            <thead></thead>
-            <tbody></tbody>
-        </table>
-        <div id="table-pagination"></div>
-        <button id="pagination" data-role="xx" type="button">asdasd</button>
-    </div>
-</x-card>
+    <x-card>
+        <div class="table-responsive">
+            <table id="table" class="display table" style="width:100%">
+                <thead>
+                    <th>Nama</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                </thead>
+                <tbody></tbody>
+            </table>
+            <div id="table-pagination"></div>
+        </div>
+    </x-card>
 
-<script>
-    $(document).ready(async function() {
-        let url = "{{ $url }}";
-        let column = [{
-                src: "name",
-                label: "Nama"
-            },
-            {
-                src: "email",
-                label: "Email"
-            },
-            {
-                src: "role",
-                label: "Role"
-            }
-        ]
+    <script>
+        $(document).ready(async function() {
+            let url = "{{ $url }}";
+            let column = ["name", "email", "role"]
 
-        let dataResponse = {};
+            let dataResponse = {};
 
-        await loadData(url);
+            await loadData(url);
 
-        async function loadData(url) {
-            dataResponse = await getData(url)
-            if (dataResponse.status_code != 200) {
-                return;
+            async function loadData(url) {
+
+                dataResponse = await getData(url)
+                console.log(dataResponse);
+
+                if (dataResponse.status_code != 200) {
+                    return;
+                }
+
+                generateBody();
+                generatePagination();
             }
 
-            generateHeader();
-            generateBody();
-            generatePagination();
-        }
+            async function generateBody() {
+                var content = "";
+                dataResponse.body.data.forEach((element) => {
+                    let body = '<tr>';
+                    column.forEach(col => {
+                        body += `<td>${element[col]}</td>`;
+                    })
+                    body += '</tr>';
+                    content += body;
+                });
 
-        function generateHeader() {
-            let header = `<tr>`;
-            column.forEach(element => {
-                header += `<th>${element.label}</th>`
-            });
+                $("#table tbody").html(content);
+            }
 
-            header += `</tr>`;
-            $("#table thead").append(header);
-        }
-
-        async function generateBody() {
-            dataResponse.body.data.forEach((element) => {
-                let body = '<tr>';
-                column.forEach(col => {
-                    body += `<td>${element[col.src]}</td>`;
-                })
-                body += '</tr>';
-
-                $("#table tbody").append(body);
-            });
-        }
-
-        async function generatePagination() {
-            var nav = `<nav aria-label="Page navigation example">
+            async function generatePagination() {
+                var nav = `<nav aria-label="Page navigation example">
                 <ul class="pagination">`;
 
-            dataResponse.body.links.forEach((element) => {
-                var label = element.label == 'pagination.previous' ?
-                    "Previous" :
-                    element.label == 'pagination.next' ?
-                    "Next" :
-                    element.label;
+                dataResponse.body.links.forEach((element) => {
+                    var label = element.label == 'pagination.previous' ?
+                        "Previous" :
+                        element.label == 'pagination.next' ?
+                        "Next" :
+                        element.label;
 
-                nav += `<li class="page-item"><button id="paginationNav" data-role="xxx" class="page-link">${label}</button></li>`;
-            });
+                    var linkUrl = url + getUrlParams(element.url);
+                    nav +=
+                        `<li class="page-item"><button data-url="${linkUrl}" class="page-link pagination-nav">${label}</button></li>`;
+                });
 
-            nav += `</ul></nav>`;
+                nav += `</ul></nav>`;
+                $("#table-pagination").html(nav);
 
-            $("#table-pagination").append(nav);
-        }
+                $(".pagination-nav").each(async function(index, element) {
+                    $(element).click(async function() {
+                        console.log(index)
+                        var url = $(element).data("url");
+                        await loadData(url)
+                    });
+                })
+            }
 
-        async function getData(fetch_url) {
-            return $.ajax({
-                url: fetch_url,
-                context: document.body
-            })
-        }
+            async function getData(fetch_url) {
+                return $.ajax({
+                    url: fetch_url,
+                    context: document.body
+                })
+            }
 
-        ;
-        $("#paginationNav").each(index => {
-            $("#paginationNav").click(function() {
-                var data = $(this).data("role");
-                console.log(index);
+            function getUrlParams(url) {
+                if (url == null) {
+                    return "";
+                }
 
-            });
+                var index = url.indexOf("?")
+                return index == -1 ? "" : url.substring(index);
+            }
         });
-
-    });
-</script>
+    </script>
 @endsection
